5bd21247c6ca8ad47557cc346030a97ea6957654
Brendan Woodell <brendan.woodell@gmail.com>
2019-01-11 15:20:03 -0800

    Finished prelim routes-ctrl-models for Users Route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const ctrl = require('../controllers/users')
+const Qctrl = require('../controllers/queue')
 
 router.post('/signup', ctrl.signup)
 
 router.get('/u', ctrl.getAll) //get All Users
-router.post('/u/:id/friends', ctrl.addFriend) //add friend, /u because I think starting with just /:id will conflict with signup
+router.post('/u/:id/friends', Qctrl.checkUser, ctrl.checkFriends, ctrl.addFriend) //add friend, /u because I think starting with just /:id will conflict with signup
 router.get('/u/:id/friends', ctrl.getFriends) //get friends list
 router.put('/u/:id', ctrl.editUser) //edit User info
 router.get('/u/:id', ctrl.getUser) //get User
