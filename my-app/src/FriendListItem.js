ec628df19f065f189ef9f5ca50cb7e77939e19c4
Dylan <kite.d92@gmail.com>
2019-01-15 11:00:07 -0800

    finalized filtering

--- my-app/src/FriendListItem.js
+++ my-app/src/FriendListItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function FriendListItem(props){
   return (
     <div className="friendListItem">
-        <div className="userImg" style={{ backgroundImage: `url("${props.img}")` }} >{props.img === '' ? props.f_name[0] : null}</div>
+        <div className="userImg" style={{ backgroundImage: `url("${props.img}")` }} >{props.img === '' ? props.username[0] : null}</div>
         <h2>{props.username}</h2>
     </div>
   )
--- my-app/src/SelectFriend.js
+++ my-app/src/SelectFriend.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SelectFriend(props){
     return(
         <div className="userResult" onClick={props.selectFriend}>
-        <div className="userImg" style={{backgroundImage: `url("${props.img}")`}} >{props.img === '' ? props.username[0] : null}</div>
+        <div className="userImg" style={{backgroundImage: `url("${props.img}")`}} >{props.img === '' ? props.username[0].toUpperCase() : null}</div>
         <p>{props.username}</p>
       </div>
     )
