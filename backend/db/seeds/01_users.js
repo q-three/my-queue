7a3ccfaaa64258a0a2b00b0db3e4b38f490f3c8d
Dylan <kite.d92@gmail.com>
2019-01-17 10:54:19 -0800

    added images to seeds

--- backend/db/seeds/01_users.js
+++ backend/db/seeds/01_users.js
@@ -4,8 +4,8 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return knex('users').insert([
         {id: 1, f_name: 'Brendan', l_name: 'Woodell', username: 'beamer92', color:'#44db23', password: '$2b$10$Qd5hkWLdCecMtukLEVGP6O9RuUcHcX5ZMeEvYDC2vsENVLNgTDV3u'},
-        {id: 2, f_name: 'Brad', l_name: 'Majors', username: 'bmajors', color:'#299cd1', password: '$2b$10$BA6Xp9.iw3wK433pNVJsQOBV9mOmiIOCuc57oBtf/TNXrHEqRnOPm'},
-        {id: 3, f_name: 'Janet', l_name: 'Weiss', username: 'janet', color:'#f442ee', password: '$2b$10$79UkVjSXD1cVOwBr/7Z58OO/r.8fWk.JxV1jWF75WJRIbFKMMgx6m'}
+        { id: 2, f_name: 'Brad', l_name: 'Majors', username: 'bmajors', color: '#299cd1', password: '$2b$10$BA6Xp9.iw3wK433pNVJsQOBV9mOmiIOCuc57oBtf/TNXrHEqRnOPm', img: 'https://assets.mycast.io/characters/6266_normal.jpg?1503951552'},
+        { id: 3, f_name: 'Janet', l_name: 'Weiss', username: 'janet', color: '#f442ee', password: '$2b$10$79UkVjSXD1cVOwBr/7Z58OO/r.8fWk.JxV1jWF75WJRIbFKMMgx6m', img: 'https://vignette.wikia.nocookie.net/rockyhorror/images/6/66/Janetport.png/revision/latest?cb=20160802162926'}
       ])
       .then(() =>{
         return knex.raw(`SELECT setval('users_id_seq', (SELECT max(id) FROM users));`)})
