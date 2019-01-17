948d94e4e748030c989dd79a2580a5b33d105362
Brendan Woodell <brendan.woodell@gmail.com>
2019-01-17 09:21:01 -0800

    redid seed passwords

--- backend/db/seeds/01_users.js
+++ backend/db/seeds/01_users.js
@@ -3,9 +3,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        {id: 1, f_name: 'Brendan', l_name: 'Woodell', username: 'beamer92', color:'#44db23', password: '$2b$10$O7QKsP01/HyQYoatiuITCe20lNEezFMDNtaOo1kSWdQb8Vd8IfPoO'}, //123123123
+        {id: 1, f_name: 'Brendan', l_name: 'Woodell', username: 'beamer92', color:'#44db23', password: '$2b$10$Zor7V9BuLUOvxzWKosYnkulIBYGYHPUiOn0LbKT0K4B3IE9kQjxeW'}, //Asd123123
         {id: 2, f_name: 'Brad', l_name: 'Majors', username: 'bmajors', color:'#299cd1', password: '$2b$10$BA6Xp9.iw3wK433pNVJsQOBV9mOmiIOCuc57oBtf/TNXrHEqRnOPm'}, //superduper
-        {id: 3, f_name: 'Janet', l_name: 'Weiss', username: 'janet', color:'#f442ee', password: '$2b$10$Jmo.mKYZUNC0yiaWdxvCXOnSmhJJJMk..k48WUKYJnanPfVv6Twmm'} //janet123
+        {id: 3, f_name: 'Janet', l_name: 'Weiss', username: 'janet', color:'#f442ee', password: '$2b$10$O4IEISVz4xAKiyPsqyuUiut/mSZTwkcOj/U2lIlCZn6ZQW9CHHbVq'} //Janet123
       ])
       .then(() =>{
         return knex.raw(`SELECT setval('users_id_seq', (SELECT max(id) FROM users));`)})
